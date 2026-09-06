82dc75a86be571359dd1dd9509f607c8e812206a
Eliya <eliya313@gmail.com>
2026-09-06 15:03:48 +0300

    added untracked files

---
+++ client/src/services/profileService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const AUTH_TOKEN_KEY = "queensMatchToken";
+
+export async function updateMentorProfile(profile) {
+  const token = window.localStorage.getItem(AUTH_TOKEN_KEY);
+
+  if (!token) {
+    throw new Error("Authentication token is missing");
+  }
+
+  const response = await axios.patch("/api/users/profile", profile, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return response.data.user;
+}
